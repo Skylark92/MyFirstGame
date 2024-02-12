965c143f1871cad9ba712d8c711fcd149631f613
Skylark92 <skylark92@live.co.kr>
2024-02-12 19:55:21 +0900

    Random Map 생성

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -1,12 +1,71 @@
-import { _decorator, Component, Node, Prefab } from "cc";
+import {
+  _decorator,
+  CCInteger,
+  Component,
+  instantiate,
+  Node,
+  Prefab,
+} from "cc";
+import { BLOCK_SIZE } from "./PlayerController";
 const { ccclass, property } = _decorator;
+
+enum BlockType {
+  BT_NONE,
+  BT_STONE,
+}
 
 @ccclass("GameManager")
 export class GameManager extends Component {
   @property({ type: Prefab })
   public boxPrefab: Prefab | null = null;
+  @property({ type: CCInteger })
+  public roadLength: number = 50;
+  private _road: BlockType[] = [];
 
-  start() {}
+  start() {
+    this.generateRoad();
+  }
 
   update(deltaTime: number) {}
+
+  generateRoad() {
+    this.node.removeAllChildren();
+
+    this._road = [];
+
+    for (let i = 1; i < this.roadLength; i++) {
+      if (this._road[i - 1] === BlockType.BT_NONE) {
+        this._road.push(BlockType.BT_STONE);
+      } else {
+        this._road.push(Math.floor(Math.random() * 2));
+      }
+    }
+
+    console.log("road: ", this._road);
+
+    for (let j = 0; j < this._road.length; j++) {
+      let block: Node | null = this.spawnBlockByType(this._road[j]);
+
+      if (block) {
+        this.node.addChild(block);
+        block.setPosition(j * BLOCK_SIZE, 0, 0);
+      }
+    }
+  }
+
+  spawnBlockByType(type: BlockType) {
+    if (!this.boxPrefab) {
+      return null;
+    }
+
+    let block: Node | null = null;
+
+    switch (type) {
+      case BlockType.BT_STONE:
+        block = instantiate(this.boxPrefab);
+        break;
+    }
+
+    return block;
+  }
 }
